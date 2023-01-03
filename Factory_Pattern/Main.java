4470abfee9769287413f5e3d76bcf71e74736480
karribalu <karri.balu123456@gmail.com>
2023-01-03 17:08:39 +0530

    third

---
+++ Factory_Pattern/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the shape you want");
+        String shape = sc.next();
+
+        ShapeFactory shapeObj = new ShapeFactory();
+        Shape obj = shapeObj.getShape(shape);
+
+        if (obj == null)
+            System.out.println("Null Object");
+        else
+            obj.draw();
+    }
+}
---
+++ Factory_Pattern/ShapeFactory.java
@@ -0,0 +1,13 @@
+public class ShapeFactory {
+    public Shape getShape(String shape) {
+        switch (shape) {
+            case "Circle":
+                return new Circle();
+            case "Rectangle":
+                return new Rectangle();
+
+            default:
+                return null;
+        }
+    }
+}
